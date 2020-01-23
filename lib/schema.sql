65bee3ff2cc8d75178be06f579ed8ccd42745dec
Chris Morgan <cwm111794@gmail.com>
2020-01-22 22:21:13 -0600

    Done.

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,3 +4,34 @@ CREATE TABLE series (
   subgenre_id INTEGER,
   id INTEGER PRIMARY KEY
 );
+
+CREATE TABLE subgenres (
+  name TEXT,
+  id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE authors (
+  name TEXT,
+  id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE books (
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER,
+  id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE characters (
+  name TEXT,
+  species TEXT,
+  motto TEXT,
+  author_id INTEGER,
+  id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE character_books (
+  id INTEGER PRIMARY KEY,
+  book_id INTEGER,
+  character_id INTEGER
+);
