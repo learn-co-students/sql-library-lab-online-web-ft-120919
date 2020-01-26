30a3ba4590bf36b981584cd8596fac204857ab4a
ricardo39985 <ricardo39985@gmail.com>
2020-01-25 21:14:59 -0500

    Done.

--- lib/schema.sql
+++ lib/schema.sql
@@ -16,16 +16,18 @@ CREATE TABLE authors (
 );
 
 CREATE TABLE books(
+    id INTEGER PRIMARY KEY,
     title TEXT,
     year INTEGER,
-    series_id INTEGER PRIMARY KEY
+    series_id INTEGER 
 );
 
 CREATE TABLE characters (
+    id INTEGER PRIMARY KEY,
     name TEXT,
     species TEXT,
     motto TEXT,
-    author_id INTEGER PRIMARY KEY
+    author_id INTEGER
 );
 
 CREATE TABLE character_books (
--- lib/update.sql
+++ lib/update.sql
@@ -1,3 +1,3 @@
 UPDATE characters
 SET species = 'Martian'
-WHERE characters.id = 8;
+WHERE id=(SELECT MAX(id) FROM characters);
